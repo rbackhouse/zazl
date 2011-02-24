15a81ab90d1711101f95f493e0ae3e69cd45fdac
Richard Backhouse <richardabackhouse@gmail.com>
2011-02-24 12:02:55 -0500

    Update build scripts to point to correct workspace directory

--- org.dojotoolkit.zazl.feature/scripts/zazlbuild.bat
+++ org.dojotoolkit.zazl.feature/scripts/zazlbuild.bat
@@ -1,5 +1,5 @@
 @echo off
 
-apache-ant-1.8.2\bin\ant -lib org.ant4eclipse_1.0.0.M4\org.ant4eclipse_1.0.0.M4.jar -lib org.ant4eclipse_1.0.0.M4\libs -Dbasedir=%CD% -f ..\workspace\org.dojotoolkit.zazl.feature\scripts\zazlbuild.xml
+apache-ant-1.8.2\bin\ant -lib org.ant4eclipse_1.0.0.M4\org.ant4eclipse_1.0.0.M4.jar -lib org.ant4eclipse_1.0.0.M4\libs -Dbasedir=%CD% -f ..\zazl\org.dojotoolkit.zazl.feature\scripts\zazlbuild.xml
 
 
