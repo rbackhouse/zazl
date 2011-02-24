852688b6abc65e47daceebb1e0d50839537cb636
Richard Backhouse <richardabackhouse@gmail.com>
2011-02-24 14:51:25 -0500

    Update build scripts to point to correct workspace directory

--- org.dojotoolkit.zazl.feature/scripts/buildzazlnodejs.sh
+++ org.dojotoolkit.zazl.feature/scripts/buildzazlnodejs.sh
@@ -8,14 +8,14 @@ mkdir jsoptimizer
 mkdir zazlutil
 mkdir -p zazlserver/lib/org/dojotoolkit/zazl/optimizer/tag
 mkdir -p zazlserver/lib/dojox/serverdtl
-cp -R ../../workspace/org.dojotoolkit.optimizer.nodejs/jsoptimizer/* ./jsoptimizer/
+cp -R ../../zazl/org.dojotoolkit.optimizer.nodejs/jsoptimizer/* ./jsoptimizer/
 mkdir ./jsoptimizer/lib/optimizer/
-cp -R ../../workspace/org.dojotoolkit.optimizer/optimizer/* ./jsoptimizer/lib/optimizer/
-cp -R ../../workspace/org.dojotoolkit.server.util.nodejs/zazlutil/* ./zazlutil/
-cp -R ../../workspace/org.dojotoolkit.zazl.nodejs/zazlserver/* ./zazlserver/
-cp ../../workspace/org.dojotoolkit.zazl.optimizer/tag/jstag.js ./zazlserver/lib/org/dojotoolkit/zazl/optimizer/tag/.
-cp ../../workspace/org.dojotoolkit.zazl/jssrc/dojox/serverdtl/Request.js ./zazlserver/lib/dojox/serverdtl/.
-cp ../../workspace/org.dojotoolkit.zazl/jssrc/dojox/serverdtl/util.js ./zazlserver/lib/dojox/serverdtl/.
+cp -R ../../zazl/org.dojotoolkit.optimizer/optimizer/* ./jsoptimizer/lib/optimizer/
+cp -R ../../zazl/org.dojotoolkit.server.util.nodejs/zazlutil/* ./zazlutil/
+cp -R ../../zazl/org.dojotoolkit.zazl.nodejs/zazlserver/* ./zazlserver/
+cp ../../zazl/org.dojotoolkit.zazl.optimizer/tag/jstag.js ./zazlserver/lib/org/dojotoolkit/zazl/optimizer/tag/.
+cp ../../zazl/org.dojotoolkit.zazl/jssrc/dojox/serverdtl/Request.js ./zazlserver/lib/dojox/serverdtl/.
+cp ../../zazl/org.dojotoolkit.zazl/jssrc/dojox/serverdtl/util.js ./zazlserver/lib/dojox/serverdtl/.
 cd ..
 tar cvzf node_modules.taz.gz node_modules/*
 rm -R zazlnodejs
@@ -29,8 +29,8 @@ cp -R ../targetplatform/dojo/plugins/org.dojotoolkit.dojo_1.5.0/dojo/* ./zazlnod
 cp -R ../targetplatform/dojo/plugins/org.dojotoolkit.dojo_1.5.0/dijit/* ./zazlnodejs/dojo/dijit/
 cp -R ../targetplatform/dojo/plugins/org.dojotoolkit.dojo_1.5.0/dojox/* ./zazlnodejs/dojo/dojox/
 mkdir -p zazlnodejs/samples
-cp -R ../workspace/org.dojotoolkit.zazl.samples/resources/* ./zazlnodejs/samples/
-cp -R ../workspace/org.dojotoolkit.zazl.nodejs/zazlserver.js ./zazlnodejs/.
+cp -R ../zazl/org.dojotoolkit.zazl.samples/resources/* ./zazlnodejs/samples/
+cp -R ../zazl/org.dojotoolkit.zazl.nodejs/zazlserver.js ./zazlnodejs/.
 mkdir -p zazlnodejs/samples/org/dojotoolkit/zazl/samples
 mkdir -p zazlnodejs/samples/org/dojotoolkit/zazl/samples/handlers
 mkdir -p zazlnodejs/samples/org/dojotoolkit/zazl/samples/filters
